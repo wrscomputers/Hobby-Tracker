e3a7036fe3de38d99e3e366b79754b90fe8fe5b2
Spencer Curtis <spencercolecurtis@gmail.com>
2020-05-12 09:55:28 -0600

    Add new Xcode projet

---
+++ HobbyTracker/HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Spencer Curtis on 5/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
