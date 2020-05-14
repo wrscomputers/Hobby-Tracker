23cbd9d77dcc05ff82f045188356306d36669d79
wrscomputers <wrscomputers@gmail.com>
2020-05-14 15:45:17 -0400

    Completed Walkthru and app

---
+++ HobbyTracker/HobbyTracker/Controller/FriendTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FriendTableViewCell.swift
+//  HobbyTracker
+//
+//  Created by Jason Hoover on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hometownLabel: UILabel!
+    @IBOutlet weak var hobbiesLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HobbyTracker/HobbyTracker/Models/Friend.swift
@@ -0,0 +1,9 @@
+//
+//  friend.swift
+//  HobbyTracker
+//
+//  Created by Jason Hoover on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
